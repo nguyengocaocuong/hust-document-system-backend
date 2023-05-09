c38fd06dd41417db4cf991d6c435749f7049d91d
nguyenngocaocuong <nguyenngocaocuong@gmail.com>
2023-05-09 22:11:52 +0700

    add docs file for test

--- src/main/java/com/hust/edu/vn/documentsystem/data/AutoImportDataForTest.java
+++ src/main/java/com/hust/edu/vn/documentsystem/data/AutoImportDataForTest.java
@@ -59,7 +59,43 @@ public class AutoImportDataForTest implements CommandLineRunner {
     @Override
     public void run(String... args) throws Exception {
         if (userRepository.findAll().size() != 0) return;
+        Random rand = new Random();
         // Tạo và insert 20 user vào database
+        List<String> avatars = Arrays.asList(
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-552.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-531.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-536.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-548.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-545-916x1024.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-534-1024x1022.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-528-1024x1022.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-537-1024x1024.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-555-972x1024.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-533-1024x976.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-547-1024x1024.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-551-1024x1024.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-540-1024x1024.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-542-1024x1024.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-546-1024x1022.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-554-1024x1024.jpg",
+                "https://funkylife.in/wp-content/uploads/2023/03/good-morning-image-530-986x1024.jpg",
+                "https://funkylife.in/wp-content/uploads/2022/09/good-morning-image-from-funkylife-436-1024x1022.jpg",
+                "https://funkylife.in/wp-content/uploads/2022/09/good-morning-image-from-funkylife-433-1022x1024.jpg",
+                "https://funkylife.in/wp-content/uploads/2022/09/good-morning-image-from-funkylife-442-1024x1024.jpg",
+                "https://funkylife.in/wp-content/uploads/2022/07/good-morning-image-funkylife-414-1024x1024.jpg",
+                "https://funkylife.in/wp-content/uploads/2021/06/good-morning-images-58.jpg",
+                "https://funkylife.in/wp-content/uploads/2021/07/good-morning-images-129.jpg",
+                "https://funkylife.in/wp-content/uploads/2021/06/good-morning-images-87.jpg",
+                "https://funkylife.in/wp-content/uploads/2021/06/good-morning-images-27.jpg",
+                "https://funkylife.in/wp-content/uploads/2021/06/good-morning-images-80.jpg",
+                "https://funkylife.in/wp-content/uploads/2021/06/good-morning-images-63.jpg",
+                "https://funkylife.in/wp-content/uploads/2021/10/good-morning-10-1024x1024.jpg");
+        List<String> docs = Arrays.asList(
+                "docs/docx.docx",
+                "docs/docx.pdf",
+                "docs/image.jpg",
+                "docs/ppt.pptx"
+        );
         List<User> users = new ArrayList<>();
         for (int i = 1; i <= 30; i++) {
             User user = new User();
@@ -67,7 +103,7 @@ public class AutoImportDataForTest implements CommandLineRunner {
             user.setEmail("user" + i + "@example.com");
             user.setPassword(passwordEncoder.encode("password" + i));
             user.setDob(new Date());
-            user.setAvatar("avatar");
+            user.setAvatar(avatars.get(rand.nextInt(avatars.size())));
             user.setPhoneNumber("0123456789");
             user.setRoleType(i == 1 ? RoleType.ADMIN : RoleType.USER);
             user.setRootPath(UUID.randomUUID().toString());
@@ -122,7 +158,7 @@ public class AutoImportDataForTest implements CommandLineRunner {
 
 
         // Imports 40 review subject
-        Random rand = new Random();
+
         List<String> reviews = Arrays.asList("<h1>Chào các bạn hôm nay tôi sẽ review về giảng viên ABC</h1><strong>cách giảng dạy,...</strong>",
                 "<h1>Review về môn Giải tích</h1><p>Thầy cô rất tận tâm và nhiệt tình trong giảng dạy</p>",
                 "<h1>Đánh giá về môn Đại số</h1><p>Cô giảng bài rất dễ hiểu và cẩn thận trong từng chi tiết</p>",
@@ -167,7 +203,7 @@ public class AutoImportDataForTest implements CommandLineRunner {
                 document.setContent("Content of document " + i);
                 document.setContentEn("Content of document " + i + " in English");
                 document.setType(DocumentType.POST_DOCUMENT);
-                document.setPath(UUID.randomUUID().toString() + "." + docTypes[i % 3]);
+                document.setPath(docs.get(rand.nextInt(docs.size())));
                 document.setCreatedAt(new Date());
                 document.setName("Document " + i + "." + docTypes[i % 5]);
                 document.setContentType("application/" + docTypes[i % 5]);
@@ -240,7 +276,7 @@ public class AutoImportDataForTest implements CommandLineRunner {
                 document.setContent("Content of document " + i);
                 document.setContentEn("Content of document " + i + " in English");
                 document.setType(DocumentType.SUBJECT_DOCUMENT);
-                document.setPath(UUID.randomUUID().toString() + "." + docTypes[i % 3]);
+                document.setPath(docs.get(rand.nextInt(docs.size())));
                 document.setCreatedAt(new Date());
                 document.setName("Document " + i + "." + docTypes[i % 5]);
                 document.setContentType("application/" + docTypes[i % 5]);
@@ -342,7 +378,7 @@ public class AutoImportDataForTest implements CommandLineRunner {
                 document.setContent("Content of document " + i);
                 document.setContentEn("Content of document " + i + " in English");
                 document.setType(DocumentType.ANSWER_POST);
-                document.setPath(UUID.randomUUID().toString() + "." + docTypes[i % 3]);
+                document.setPath(docs.get(rand.nextInt(docs.size())));
                 document.setCreatedAt(new Date());
                 document.setName("Document " + i + "." + docTypes[i % 5]);
                 document.setContentType("application/" + docTypes[i % 5]);
