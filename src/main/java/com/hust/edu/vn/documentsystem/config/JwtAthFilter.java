07165a7b23a2278a1ffa563cc1038c6b774421b2
Nguyen Ngo Cao Cuong <nguyenngocaocuong@gmail.com>
2023-05-11 08:55:53 +0700

    enable sercurity

--- src/main/java/com/hust/edu/vn/documentsystem/config/JwtAthFilter.java
+++ src/main/java/com/hust/edu/vn/documentsystem/config/JwtAthFilter.java
@@ -1,65 +1,62 @@
 package com.hust.edu.vn.documentsystem.config;
-//
-//import com.hust.edu.vn.documentsystem.service.impl.CustomUserDetailsService;
-//import com.hust.edu.vn.documentsystem.utils.JwtUtils;
-//import jakarta.servlet.FilterChain;
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
-//import lombok.NonNull;
-//import lombok.extern.slf4j.Slf4j;
-//import org.springframework.beans.factory.annotation.Autowired;
-//import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
-//import org.springframework.security.core.context.SecurityContextHolder;
-//import org.springframework.security.core.userdetails.UserDetails;
-//import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
-//import org.springframework.stereotype.Component;
-//import org.springframework.web.filter.OncePerRequestFilter;
-//
-//import java.io.IOException;
-//
-//@Component
-//@Slf4j
-//public class JwtAthFilter extends OncePerRequestFilter {
-//    private final CustomUserDetailsService customUserDetailsService;
-//    private final JwtUtils jwtUtils;
-//
-//    @Autowired
-//    public JwtAthFilter(CustomUserDetailsService customUserDetailsService, JwtUtils jwtUtils) {
-//        this.customUserDetailsService = customUserDetailsService;
-//        this.jwtUtils = jwtUtils;
-//    }
-//
-//    @Override
-//    protected void doFilterInternal(
-//            HttpServletRequest request,
-//            @NonNull HttpServletResponse response,
-//            @NonNull FilterChain filterChain
-//    ) throws ServletException, IOException {
-//        String authHeader = request.getHeader("Authorization");
-//        final String userEmail;
-//        final String jwtToken;
-//        if (authHeader == null && request.getRequestURI().startsWith("/api/v1/socket")) {
-//            authHeader = "Bearer " + request.getParameter("token");
-//        }
-//        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-//            filterChain.doFilter(request, response);
-//            return;
-//        }
-//        jwtToken = authHeader.substring(7);
-//        userEmail = jwtUtils.extractUserName(jwtToken);
-//        log.info(userEmail);
-//        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
-//            UserDetails userDetails = customUserDetailsService.loadUserByUsername(userEmail);
-//            if (userDetails.isEnabled() && jwtUtils.isTokenValid(jwtToken, userDetails)) {
-//                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
-//                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-//                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
-//            }
-//            filterChain.doFilter(request, response);
-//        }
-//    }
-//}
-public class JwtAthFilter{
 
+import com.hust.edu.vn.documentsystem.service.impl.CustomUserDetailsService;
+import com.hust.edu.vn.documentsystem.utils.JwtUtils;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import lombok.NonNull;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.authentication.WebAuthenticationDetailsSource;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+@Slf4j
+public class JwtAthFilter extends OncePerRequestFilter {
+    private final CustomUserDetailsService customUserDetailsService;
+    private final JwtUtils jwtUtils;
+
+    @Autowired
+    public JwtAthFilter(CustomUserDetailsService customUserDetailsService, JwtUtils jwtUtils) {
+        this.customUserDetailsService = customUserDetailsService;
+        this.jwtUtils = jwtUtils;
+    }
+
+    @Override
+    protected void doFilterInternal(
+            HttpServletRequest request,
+            @NonNull HttpServletResponse response,
+            @NonNull FilterChain filterChain
+    ) throws ServletException, IOException {
+        String authHeader = request.getHeader("Authorization");
+        final String userEmail;
+        final String jwtToken;
+        if (authHeader == null && request.getRequestURI().startsWith("/api/v1/socket")) {
+            authHeader = "Bearer " + request.getParameter("token");
+        }
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        jwtToken = authHeader.substring(7);
+        userEmail = jwtUtils.extractUserName(jwtToken);
+        log.info(userEmail);
+        if (userEmail != null && SecurityContextHolder.getContext().getAuthentication() == null) {
+            UserDetails userDetails = customUserDetailsService.loadUserByUsername(userEmail);
+            if (userDetails.isEnabled() && jwtUtils.isTokenValid(jwtToken, userDetails)) {
+                UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(userDetails, null, userDetails.getAuthorities());
+                authenticationToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+            }
+            filterChain.doFilter(request, response);
+        }
+    }
 }
