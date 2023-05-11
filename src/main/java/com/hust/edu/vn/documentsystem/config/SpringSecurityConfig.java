c35a0bd3d232845f1ade99018ecd8a41e043466c
Nguyen Ngo Cao Cuong <nguyenngocaocuong@gmail.com>
2023-05-11 08:36:30 +0700

    fix cors

--- src/main/java/com/hust/edu/vn/documentsystem/config/SpringSecurityConfig.java
+++ src/main/java/com/hust/edu/vn/documentsystem/config/SpringSecurityConfig.java
@@ -43,50 +43,50 @@ public class SpringSecurityConfig {
 
     @Bean
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        http
-//                .cors()
-//                .configurationSource(new CorsConfigurationSource() {
-//                    @Override
-//                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
-//                        CorsConfiguration corsConfiguration = new CorsConfiguration();
-//                        corsConfiguration.setAllowedOrigins(Collections.singletonList(System.getenv("FRONTEND_URL")));
-//                        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
-//                        corsConfiguration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
-//                        corsConfiguration.setExposedHeaders(Collections.singletonList("Authorization"));
-//                        corsConfiguration.setAllowCredentials(false);
-//                        corsConfiguration.setMaxAge(Duration.ofMinutes(20));
-//                        return corsConfiguration;
-//                    }
-//                })
-//                .and()
-//                .csrf()
-//                .disable();
-//
-//        http.sessionManagement()
-//                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-//
-//        /* config public api*/
-//        http.authorizeHttpRequests()
-//                .requestMatchers(WHITE_LIST_URLS)
-//                .permitAll();
-//
-//        http.authorizeHttpRequests()
-//                .requestMatchers("/api/v1/admins/**","/api/v1/socket/admins/**")
-//                .hasAuthority("ADMIN");
-//
-//        http.authorizeHttpRequests()
-//                .requestMatchers("/api/v1/users/**","/api/v1/socket/users/**")
-//                .hasAuthority("USER");
-//
-//        http.authorizeHttpRequests()
-//                .requestMatchers("/api/v1/generals/**")
-//                .hasAnyAuthority("USER", "ADMIN");
-//
-//
-//        http.addFilterBefore(jwtAthFilter, UsernamePasswordAuthenticationFilter.class);
-//
-//        http.httpBasic();
-        http.cors().and().csrf().disable().authorizeHttpRequests().anyRequest().permitAll();
+        http
+                .cors()
+                .configurationSource(new CorsConfigurationSource() {
+                    @Override
+                    public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
+                        CorsConfiguration corsConfiguration = new CorsConfiguration();
+                        corsConfiguration.setAllowedOrigins(Arrays.asList("FRONTEND_URL","https://view.officeapps.live.com/op/embed.aspx"));
+                        corsConfiguration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "DELETE", "PATCH"));
+                        corsConfiguration.setAllowedHeaders(Arrays.asList("Content-Type", "Authorization"));
+                        corsConfiguration.setExposedHeaders(Collections.singletonList("Authorization"));
+                        corsConfiguration.setAllowCredentials(false);
+                        corsConfiguration.setMaxAge(Duration.ofMinutes(20));
+                        return corsConfiguration;
+                    }
+                })
+                .and()
+                .csrf()
+                .disable();
+
+        http.sessionManagement()
+                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        /* config public api*/
+        http.authorizeHttpRequests()
+                .requestMatchers(WHITE_LIST_URLS)
+                .permitAll();
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/api/v1/admins/**","/api/v1/socket/admins/**")
+                .hasAuthority("ADMIN");
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/api/v1/users/**","/api/v1/socket/users/**")
+                .hasAuthority("USER");
+
+        http.authorizeHttpRequests()
+                .requestMatchers("/api/v1/generals/**")
+                .hasAnyAuthority("USER", "ADMIN");
+
+
+        http.addFilterBefore(jwtAthFilter, UsernamePasswordAuthenticationFilter.class);
+
+        http.httpBasic();
+
         return http.build();
 
     }
