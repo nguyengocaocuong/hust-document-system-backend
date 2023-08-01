b69cd1a560ee536a47f39355eeadfc0b52f211a1
nguyenngocaocuong <nguyenngocaocuong@gmail.com>
2023-08-01 12:02:18 +0700

    config env

--- src/main/java/com/hust/edu/vn/documentsystem/config/PusherConfig.java
+++ src/main/java/com/hust/edu/vn/documentsystem/config/PusherConfig.java
@@ -6,10 +6,10 @@ import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class PusherConfig {
-    private final String appId = System.getenv("1523503");
-    private final String appKey = System.getenv("070ff19e8a1a4c8d4553");
-    private final String appSecret = System.getenv("88eac97d6be356957e07");
-    private final String appCluster = System.getenv("ap1");
+    private final String appId = "1523503";
+    private final String appKey = "070ff19e8a1a4c8d4553";
+    private final String appSecret = "88eac97d6be356957e07";
+    private final String appCluster = "ap1";
 
     @Bean
     public Pusher pusher(){
